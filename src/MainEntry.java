8c3f9caba53decc9ff7abc1f168ab14c396e76ca
ithenry <55816847+ithenry@users.noreply.github.com>
2019-10-07 21:19:40 -0700

    Initial commit

---
+++ src/MainEntry.java
@@ -0,0 +1,30 @@
+//HEADER
+//Program Name: 2D Array
+//Author: Collaborative between class, Isaak Henry participating
+//Class: CS260 Fall 2019
+//Date: 10/7/2019
+//Description: This program uses 2D arrays to print out an image of a triangle and a glass made of asterisks.
+
+package edu.cgcc.cs260;
+
+public class MainEntry {
+
+    public static void main(String[] args) {
+        //create and fill 2D array to print image
+        TwoDArray arr = new TwoDArray();
+        arr.printArray();
+
+    }
+
+}
+
+/* FOOTER
+ *       ********
+ **       ******
+ ***       ****
+ ****       **
+ *****      **
+ ******     **
+ *******    **
+ ********  ****
+ */
